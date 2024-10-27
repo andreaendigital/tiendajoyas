19c465ecde1949eeef3c736bdf5d9d7a68274a1e
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-10-27 11:57:29 -0300

    funcion obtener joyas

--- consultas.js
+++ consultas.js
@@ -9,3 +9,56 @@ const pool = new Pool({
   database: "joyas",
   allowExitOnIdle: true,
 });
+
+const obtenerJoyas = async ({ limits = 6, order_by = "id_ASC", page = 1 }) => {
+    const [columna, orden] = order_by.split("_");
+    const offset = (page - 1) * limits;
+  
+    // Validación de columnas y órdenes permitidas
+    const columnasPermitidas = [
+      "id",
+      "nombre",
+      "categoria",
+      "metal",
+      "precio",
+      "stock",
+    ];
+    const ordenesPermitidas = ["ASC", "DESC"];
+  
+    if (
+      !columnasPermitidas.includes(columna) ||
+      !ordenesPermitidas.includes(orden)
+    ) {
+      throw new Error("Parámetro order_by no válido");
+      }
+
+        // Consulta para obtener las joyas según el orden y paginación
+  const consulta = format(
+    "SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s",
+    columna,
+    orden,
+    limits,
+    offset
+  );
+
+  const { rows: joyas } = await pool.query(consulta);
+
+    // Calcular el stock total dinámicamente
+    const stockTotal = joyas.reduce((total, joya) => total + joya.stock, 0);
+
+    // Construir la estructura HATEOS
+    const joyasConLinks = joyas.map((joya) => ({
+      name: joya.nombre,
+      href: `/joyas/joya/${joya.id}`,
+    }));
+  
+    return {
+      total: joyas.length,
+      stockTotal, // Aquí agregamos el stock total dinámico
+      results: joyasConLinks,
+    };
+  };
+
+
+
+  module.exports = { obtenerJoyas };
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express"); // importamos express
 const cors = require("cors"); // importamos cors
 const app = express(); //Creamos una instancia de express
 
+const { obtenerJoyas} = require("./consultas");
+
 app.listen(3000, () => console.log("Servidor arriba en el puerto 3000 ;)"));
 
 app.use(express.json()); //middleware
